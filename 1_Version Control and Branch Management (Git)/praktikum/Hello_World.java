ad9a05f99db518a3259395ffecfe87ec5a162b9c
Fel-1 <felixreinaldosiong@yahoo.co.id>
2022-02-22 12:22:42 +0700

    inisialisasi project

---
+++ 1_Version Control and Branch Management (Git)/praktikum/Hello_World.java
@@ -0,0 +1,12 @@
+package praktikum;
+
+/**
+ * Hello World
+ */
+public class Hello_World {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Tugas Section 1");
+    }
+}
